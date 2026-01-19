bc0bcc049ee657801308fccf78b0436f40c4a73b
SADHIKA TS <tsssadhika@gmail.com>
2026-01-19 23:37:09 +0530

    Time: 24 ms (25.61%) | Memory: 76.9 MB (83.39%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -10,22 +10,10 @@ class Solution {
         for(int i=0;i<nums.length-1;i++)
         {
             if(nums[i]==nums[i+1])
-             return true;
-            
+             return true;            
             
         }
         return false;
     }
-    public static void main(String[] args)
-    {
-        Scanner s=new Scanner(System.in);
-        int size=s.nextInt();
-        int[] nums=new int[size];
-        for(int i=0;i<size;i++)
-        {
-            nums[i]=s.nextInt();
-        }
-        boolean contains=containsDuplicate(nums);
-        System.out.println(contains);
-    }
+    
 }
