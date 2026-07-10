ab7d21ebe2e90365b67347bfff175ca7384c1586
SADHIKA TS <tsssadhika@gmail.com>
2026-07-10 16:05:15 +0530

    Time: 3 ms (60.52%) | Memory: 44.4 MB (57.24%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,57 +1,60 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
-        int count=0;
+        // int count=0;
         s=s.toLowerCase();
 
         
 
-        // int left=0;
-        // int right=sa.length()-1;
-
-        // while(left<right)
-        // {
-        //     if(!Character.isLetterOrDigit(sa.charAt(left)))
-        //     left++;
-        //     else if(!Character.isLetterOrDigit(sa.charAt(right)))
-        //     right--;
-        //     else
-        //     {
-        //         count++;
-        //         left++;
-        //         right--;
-        //     }
-        // }
-
-
-        StringBuilder org=new StringBuilder();
-
-        for(int i=0;i<s.length();i++)
-        {
-            char ch=s.charAt(i);
-            if(Character.isLetterOrDigit(ch))
-            {
-                org.append(ch);
-            }
-        }
-        
-
-        int left=0,right=org.length()-1;
+        int left=0;
+        int right=s.length()-1;
 
         while(left<right)
         {
-            if(org.charAt(left)==org.charAt(right))
+            if(!Character.isLetterOrDigit(s.charAt(left)))
+            left++;
+            else if(!Character.isLetterOrDigit(s.charAt(right)))
+            right--;
+            else if(s.charAt(left)==s.charAt(right))
             {
+                
                 left++;
                 right--;
             }
             else
-            {
-                return false;
-
-            }
+            return false;
         }
         return true;
+
+
+        // StringBuilder org=new StringBuilder();
+
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     char ch=s.charAt(i);
+        //     if(Character.isLetterOrDigit(ch))
+        //     {
+        //         org.append(ch);
+        //     }
+        // }
+        
+
+        // int left=0,right=org.length()-1;
+
+        // while(left<right)
+        // {
+        //     if(org.charAt(left)==org.charAt(right))
+        //     {
+        //         left++;
+        //         right--;
+        //     }
+        //     else
+        //     {
+        //         return false;
+
+        //     }
+        // }
+        // return true;
 
         
 
