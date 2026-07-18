9cf2e69f7a92060a8419672a7faa0729ee40ea0e
SADHIKA TS <tsssadhika@gmail.com>
2026-07-18 19:31:54 +0530

    Time: 164 ms (5.08%) | Memory: 47 MB (5.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -32,36 +32,6 @@ class Solution {
             }
          }
 
-
-
-    //     char[] ch=s.toCharArray();
-    //     char[] cht=t.toCharArray();
-
-    //     Arrays.sort(ch);
-    //     Arrays.sort(cht);
-
-    //     if(ch.length!=cht.length)
-    //     return false;
-
-    
-
-    //     for(int i=0;i<ch.length;i++)
-    //     {
-    //         if(ch[i]==cht[i])
-    //         {
-    //         //     System.out.println("ch:"+ch[i]);
-    //         // System.out.println("cht:"+cht[i]);
-    //         bool=true;
-    //         }
-            
-    //         else 
-    //         {
-    //             bool=false;
-    //             break;
-    //         }
-            
-            
-    //     }
         return true;
         
     }
