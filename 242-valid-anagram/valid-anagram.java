4fc8225980ebdb25002c225a50a7dd11724a1260
SADHIKA TS <tsssadhika@gmail.com>
2026-07-18 19:19:37 +0530

    Time: 162 ms (5.08%) | Memory: 47.1 MB (5.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,33 +1,66 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] ch=s.toCharArray();
-        char[] cht=t.toCharArray();
+         boolean bool=true;
 
-        Arrays.sort(ch);
-        Arrays.sort(cht);
-
-        if(ch.length!=cht.length)
-        return false;
-
-        boolean bool=true;
-
-        for(int i=0;i<ch.length;i++)
-        {
-            if(ch[i]==cht[i])
+         int[] arr=new int[128];
+         if(s.length()!=t.length()) return false;
+         else
+         {
+            
+            for(int i=0;i<s.length();i++)
             {
-            //     System.out.println("ch:"+ch[i]);
-            // System.out.println("cht:"+cht[i]);
-            bool=true;
+                int ascii=(int)s.charAt(i);
+                arr[ascii]+=1;
+                System.out.println(ascii);
             }
-            
-            else 
+            for(int i=0;i<t.length();i++)
             {
-                bool=false;
-                break;
+                int ascii=(int)t.charAt(i);
+                arr[ascii]-=1;
+
             }
+
+            for(int i=97;i<arr.length;i++)
+            {
+                if(arr[i]<0 || arr[i]==1)
+                {
+                    bool=false;
+                    break;
+                }
+                
+            }
+         }
+
+
+
+    //     char[] ch=s.toCharArray();
+    //     char[] cht=t.toCharArray();
+
+    //     Arrays.sort(ch);
+    //     Arrays.sort(cht);
+
+    //     if(ch.length!=cht.length)
+    //     return false;
+
+    
+
+    //     for(int i=0;i<ch.length;i++)
+    //     {
+    //         if(ch[i]==cht[i])
+    //         {
+    //         //     System.out.println("ch:"+ch[i]);
+    //         // System.out.println("cht:"+cht[i]);
+    //         bool=true;
+    //         }
+            
+    //         else 
+    //         {
+    //             bool=false;
+    //             break;
+    //         }
             
             
-        }
+    //     }
         return bool;
         
     }
