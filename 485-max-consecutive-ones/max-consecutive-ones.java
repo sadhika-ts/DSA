158103f33e37a305f9a08e7a51671c39a32efeb8
SADHIKA TS <tsssadhika@gmail.com>
2026-08-01 12:58:46 +0530

    Time: 4 ms (4.78%) | Memory: 52.4 MB (84.58%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -21,22 +21,7 @@ class Solution {
             }
         }
         
-        // int max=0;        
-        
-        // for(int i=0;i<nums.length;i++)
-        // {
-        //     if(1==nums[i])
-        //     {
-        //         max=max+1;
-        //     }
-        //     else
-        //     {
-        //         maxsum=Math.max(maxsum,max);                
-        //         max=0;
-        //     }
-            
-        //     maxsum=Math.max(maxsum,max);
-        // }
+      
         
          return maxsum;
     }
